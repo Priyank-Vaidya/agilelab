ac9c901bfc7e72c8800a7c67906a657ab6407cdd
Priyank Vaidya <priyankvaidya09@gmail.com>
2024-11-12 23:07:04 +0530

    Flask app

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
- return 'Hello, World from next commit'
+ return 'Hello, World from next commit from here'
 if __name__ == '__main__':
  app.run(debug=True)
 
